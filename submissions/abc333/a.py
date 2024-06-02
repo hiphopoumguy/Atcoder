956b2fbc49535453d7d2f37a60f5d45b25a82cba
hiphopoumguy <study0207sodai@gmail.com>
2024-06-02 23:20:09 +0900

    add submission: 2024/06/02 23:20:08

---
+++ submissions/abc333/a.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+import sys
+import math
+import bisect
+from heapq import heapify, heappop, heappush
+from collections import deque, defaultdict, Counter
+from functools import cache
+from fractions import Fraction
+from itertools import accumulate, combinations, permutations, product
+from sortedcontainers import SortedSet, SortedList, SortedDict
+mod = 998244353
+n = input()
+ans = []
+for _ in range(int(n)):
+    ans.append(n)
+    
+print(''.join(ans))
