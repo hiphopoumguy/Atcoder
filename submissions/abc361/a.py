189bd6a5270be1ff4b85be26e950be7a5b0bdd15
S.YOKOYAMA <study0207sodai@gmail.com>
2024-07-12 13:34:24 +0900

    add submission: 2024/07/12 13:34:02

---
+++ submissions/abc361/a.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+import sys
+import math
+import bisect
+from heapq import heapify, heappop, heappush
+from collections import deque, defaultdict, Counter
+from functools import cache
+from fractions import Fraction
+from itertools import accumulate, combinations, permutations, product
+from sortedcontainers import SortedSet, SortedList, SortedDict
+mod = 998244353
+n, k, x = map(int, input().split())
+a = list(map(int, input().split()))
+
+a.insert(k, x)
+print(*a)
