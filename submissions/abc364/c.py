e4cab87529af05419e5ccabed9c0c41d3ceb8f5f
S.YOKOYAMA <study0207sodai@gmail.com>
2024-08-02 16:41:31 +0900

    add submission: 2024/08/02 16:41:26

---
+++ submissions/abc364/c.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+import sys
+import math
+import bisect
+from heapq import heapify, heappop, heappush
+from collections import deque, defaultdict, Counter
+from functools import cache
+from fractions import Fraction
+from itertools import accumulate, combinations, permutations, product
+from sortedcontainers import SortedSet, SortedList, SortedDict
+mod = 998244353
+N, X, Y = map(int, input().split())
+A = list(map(int, input().split()))
+B = list(map(int, input().split()))
+
+A.sort(reverse=True)
+B.sort(reverse=True)
+a = 0
+b = 0
+for i in range(N):
+    a += A[i]
