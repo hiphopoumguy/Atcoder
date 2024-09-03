ffaee52f6f53cf3bdae01b93f1fa5ddc49996b56
S.YOKOYAMA <study0207sodai@gmail.com>
2024-09-03 22:55:30 +0900

    add submission: 2024/09/03 22:55:13

---
+++ submissions/abc369/c.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+import sys
+import math
+import bisect
+from heapq import heapify, heappop, heappush
+from collections import deque, defaultdict, Counter
+from functools import cache
+from fractions import Fraction
+from itertools import accumulate, combinations, permutations, product
+from sortedcontainers import SortedSet, SortedList, SortedDict
+mod = 998244353
+N = int(input())
+A = list(map(int, input().split()))
+
+ans = N
+diff = []
+for i in range(1, N):
+    diff.append(A[i]-A[i-1])
+
+last = 10**9
+length = 1
