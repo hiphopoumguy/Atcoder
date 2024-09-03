9c4b1d06e03890e7a0160b83212e055e3a645522
S.YOKOYAMA <study0207sodai@gmail.com>
2024-09-03 22:55:13 +0900

    add submission: 2024/09/03 22:55:13

---
+++ submissions/abc368/a.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+import sys
+import math
+import bisect
+from heapq import heapify, heappop, heappush
+from collections import deque, defaultdict, Counter
+from functools import cache
+from fractions import Fraction
+from itertools import accumulate, combinations, permutations, product
+from sortedcontainers import SortedSet, SortedList, SortedDict
+mod = 998244353
+N, K = map(int, input().split())
+A = list(map(int, input().split()))
+
+ans = A[-K:] + A[:N-K]
+print(*ans)
