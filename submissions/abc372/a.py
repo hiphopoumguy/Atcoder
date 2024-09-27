6369914eb9d11112015aad024e0942414e4a1671
S.YOKOYAMA <study0207sodai@gmail.com>
2024-09-27 17:47:19 +0900

    add submission: 2024/09/27 17:47:11

---
+++ submissions/abc372/a.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+import sys
+import math
+import bisect
+from heapq import heapify, heappop, heappush
+from collections import deque, defaultdict, Counter
+from functools import cache
+from fractions import Fraction
+from itertools import accumulate, combinations, permutations, product
+from sortedcontainers import SortedSet, SortedList, SortedDict
+mod = 998244353
+S = input()
+ans = []
+for s in S:
+  if s != '.':
+    ans += s
+print("".join(ans))
